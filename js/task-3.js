2d70f478571cc8894f74dbdcfb56edfadfb91c1e
Iryna Liskevych <160422110+Ira0000@users.noreply.github.com>
2024-06-23 18:43:40 +0200

    do 3 tasks in homework

---
+++ js/task-3.js
@@ -0,0 +1,29 @@
+class StringBuilder {
+  #value;
+  constructor(initialValue) {
+    this.#value = initialValue || '';
+  }
+  // повертає поточне значення приватної властивості value.
+  getValue() {
+    return this.#value;
+  }
+  // отримує параметр str (рядок) і додає його в кінець значення приватної властивості value об'єкта, який викликає цей метод.
+  padEnd(str) {
+    this.#value = this.#value + str;
+  }
+  // отримує параметр str (рядок) і додає його на початок значення приватної властивості value об'єкта, який викликає цей метод.
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+  padBoth(str) {
+    this.#value = str + this.#value + str;
+  }
+}
+const builder = new StringBuilder('.');
+console.log(builder.getValue()); // "."
+builder.padStart('^');
+console.log(builder.getValue()); // "^."
+builder.padEnd('^');
+console.log(builder.getValue()); // "^.^"
+builder.padBoth('=');
+console.log(builder.getValue()); // "=^.^="
